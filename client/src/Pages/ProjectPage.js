7e7704d021893ac3464378a8a7c4d6ee133d5034
Robert <robertschauss91@gmail.com>
2023-05-02 10:36:06 -0700

    deploying again

--- client/src/Pages/ProjectPage.js
+++ client/src/Pages/ProjectPage.js
@@ -7,7 +7,7 @@ import {
 } from "react-bootstrap";
 import { useQuery } from "@apollo/client";
 import { PROJECTS_QUERY } from "../utils/queries";
-import {ProjectForm} from "../components/Projects";
+import { ProjectForm } from "../components/Projects/ProjectForm";
 import ProjectList from "../components/Projects/ProjectList";
 
 const ProjectPage = () =>  {
