596e096a226e0209bab275524c8ee823023ac4b4
Robert <robertschauss91@gmail.com>
2023-05-01 21:34:43 -0700

    another import change

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { HomePage, ProjectPage, DailyLogPage } from "./pages/index";
+import { HomePage, ProjectPage, DailyLogPage } from "./pages/index.js";
 import { UserProfile, LoginForm, RegisterForm } from "./components/Users";
-import { MyNav, Footer } from "./components/Navigation/index";
+import { MyNav, Footer } from "./components/Navigation/index.js";
 import {
   ApolloClient,
   InMemoryCache,
