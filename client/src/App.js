4781227b5f6a992d2b02c116a2ff2cf88246b7fa
Robert <robertschauss91@gmail.com>
2023-05-01 21:28:50 -0700

    another change to imports in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { HomePage, ProjectPage, DailyLogPage } from "./pages";
+import { HomePage, ProjectPage, DailyLogPage } from "./pages/index";
 import { UserProfile, LoginForm, RegisterForm } from "./components/Users";
 import { MyNav, Footer } from "./components/Navigation/index";
 import {
