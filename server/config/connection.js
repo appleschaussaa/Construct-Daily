cf1984966e95fa0feffad95e683818cbf2a1bad1
Robert <robertschauss91@gmail.com>
2023-05-01 22:40:41 -0700

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,10 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
+
+// mongoose.connect(
+//   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/constructDaily"
+// );
+
+// module.exports = mongoose.connection;
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/constructDaily"
-);
-
-module.exports = mongoose.connection;
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/UW-Bootcamp');
