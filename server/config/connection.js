58c9ecf23fc8c64cb5c1d30feba566160e2626a7
Robert <robertschauss91@gmail.com>
2023-05-02 00:18:04 -0700

    test deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/constructDaily"
-);
+// mongoose.connect(
+//   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/constructDaily"
+// );
 
-module.exports = mongoose.connection;
+// module.exports = mongoose.connection;
 
-// mongoose.createConnection(
-//   process.env.MONGODB_URI || 'mongodb+srv://robertschauss91:<GxBQjNChYq2RCYUQ>@uw-bootcamp.uyxhzht.mongodb.net/?retryWrites=true&w=majorityp');
+mongoose.createConnection(
+  process.env.MONGODB_URI || 'mongodb+srv://robertschauss91:<GxBQjNChYq2RCYUQ>@uw-bootcamp.uyxhzht.mongodb.net/?retryWrites=true&w=majorityp');
 
-  // module.exports = mongoose.connection;
+  module.exports = mongoose.connection;
