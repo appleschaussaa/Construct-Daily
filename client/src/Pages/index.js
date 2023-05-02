04e9245c384f0b48119454f3754de95a6c69b820
Robert <robertschauss91@gmail.com>
2023-05-01 23:33:49 -0700

    deploying

--- client/src/Pages/index.js
+++
@@ -1,3 +0,0 @@
-export { default as HomePage } from "./HomePage";
-export { default as DailyLogPage } from "./DailyLogPage";
-export { default as ProjectPage } from "./ProjectPage";
--- server/config/connection.js
+++ server/config/connection.js
@@ -7,4 +7,6 @@ const mongoose = require("mongoose");
 // module.exports = mongoose.connection;
 
 mongoose.createConnection(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/UW-Bootcamp');
+  process.env.MONGODB_URI || 'mongodb+srv://robertschauss91:<GxBQjNChYq2RCYUQ>@uw-bootcamp.uyxhzht.mongodb.net/UW-Bootcamp?retryWrites=true&w=majorityp');
+
+  module.exports = mongoose.connection;
